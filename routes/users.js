13ee7d3e430f78735a2f89c42e98fa3c23ebb4d7
Administrator <lionel@buzzy.cc>
2018-10-26 14:03:13 +0800

    接口

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,7 @@ router.get('/', function(req, res, next) {
 router.get('/get_zkl_js',function(req,res,next){
 	//console.log('========get_zkl_js=======')
 	var key = req.query.tag?req.query.tag:'380';
-	console.log(key)
+	
 	mem.get('get_zkl_js_liujiazhi'+key).then(function(value){
 		//console.log('========get_zkl_js mem=======');
 		if(value){
@@ -32,6 +32,36 @@ router.get('/get_zkl_js',function(req,res,next){
 		            mem.set('get_zkl_js_liujiazhi',JSON.stringify(zkl),29).then(function(){});
 					var text = 'get_zkl_js('+JSON.stringify(zkl)+')';
 					res.send(text)
+				}catch(e){
+					//console.log(e)
+					res.send('error');
+				}
+			});
+		}
+	})
+});
+
+router.get('/get_zkls',function(req,res,next){
+	//console.log('========get_zkl_js=======')
+	var key = req.query.tag?req.query.tag:'A5E';
+	
+	mem.get('get_zkl_js_liujiazhi'+key).then(function(value){
+		//console.log('========get_zkl_js mem=======');
+		if(value){
+			res.send(value)
+		}else{
+			var url = "https://alipay.yuchuantech.com/zkl/zkl.php?qudao=liujiazhi"
+			request({url: url, timeout: 3000}, function (err, response, body) {
+				if(err){
+					console.log(err)
+					return res.send('error');
+				}
+				try{
+		            var data = JSON.parse(body);
+		            var zkl = [];
+		            zkl = data[key].zkl;
+		            mem.set('get_zkl_js_liujiazhi',JSON.stringify(zkl),29).then(function(){});
+					res.send(JSON.stringify(zkl))
 				}catch(e){
 					//console.log(e)
 					res.send('error');
