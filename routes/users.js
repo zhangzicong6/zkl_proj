7763d825dc5ee24274f6ef3822ed9ea0d25d79eb
Administrator <lionel@buzzy.cc>
2018-10-26 13:40:38 +0800

    Refresh

--- routes/users.js
+++ routes/users.js
@@ -11,7 +11,9 @@ router.get('/', function(req, res, next) {
 
 router.get('/get_zkl_js',function(req,res,next){
 	//console.log('========get_zkl_js=======')
-	mem.get('get_zkl_js_liujiazhi').then(function(value){
+	var key = req.query.tag?req.query.tag:'380';
+	console.log(key)
+	mem.get('get_zkl_js_liujiazhi'+key).then(function(value){
 		//console.log('========get_zkl_js mem=======');
 		if(value){
 			var text = 'get_zkl_js('+value+')';
@@ -25,10 +27,8 @@ router.get('/get_zkl_js',function(req,res,next){
 				}
 				try{
 		            var data = JSON.parse(body);
-		            var zkl;
-		            for (var key in data) {
-		            	zkl = data[key].zkl
-		            }
+		            var zkl = [];
+		            zkl = data[key].zkl;
 		            mem.set('get_zkl_js_liujiazhi',JSON.stringify(zkl),29).then(function(){});
 					var text = 'get_zkl_js('+JSON.stringify(zkl)+')';
 					res.send(text)
