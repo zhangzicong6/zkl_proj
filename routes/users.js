ce5fe2ad53e5819cdf30a527987b0a317a9faf7a
Administrator <lionel@buzzy.cc>
2018-11-07 17:11:17 +0800

    Ok

--- routes/users.js
+++ routes/users.js
@@ -44,7 +44,6 @@ router.get('/get_zkl_js',function(req,res,next){
 router.get('/get_zkls',function(req,res,next){
 	//console.log('========get_zkl_js=======')
 	var key = req.query.tag?req.query.tag:'A5E';
-	
 	mem.get('get_zkl_js_liujiazhi'+key).then(function(value){
 		//console.log('========get_zkl_js mem=======');
 		if(value){
@@ -60,7 +59,7 @@ router.get('/get_zkls',function(req,res,next){
 		            var data = JSON.parse(body);
 		            var zkl = [];
 		            zkl = data[key].zkl;
-		            mem.set('get_zkl_js_liujiazhi',JSON.stringify(zkl),29).then(function(){});
+		            mem.set('get_zkl_js_liujiazhi'+key,JSON.stringify(zkl),29).then(function(){});
 					res.send(JSON.stringify(zkl))
 				}catch(e){
 					//console.log(e)
@@ -70,5 +69,63 @@ router.get('/get_zkls',function(req,res,next){
 		}
 	})
 });
+
+
+router.get('/get_ali_zkl',function(req,res,next){
+	mem.get('get_ali_zkl_liujiazhi').then(function(value){
+		//console.log('========get_zkl_js mem=======');
+		if(value){
+			res.send(value)
+		}else{
+			var url = "https://www.guojucloud.com/zkl/ali/xieb.json";
+			request({url: url, timeout: 3000}, function (err, response, body) {
+				if(err){
+					console.log(err)
+					return res.send('error');
+				}
+				try{
+					body = body.split('(')[1].split(')')[0]
+		            var data = JSON.parse(body);
+		            var zkl = [];
+		            zkl = data["code"];
+		            mem.set('get_ali_zkl_liujiazhi',JSON.stringify(zkl),60).then(function(){});
+					res.send(JSON.stringify(zkl))
+				}catch(e){
+					//console.log(e)
+					res.send('error');
+				}
+			});
+		}
+	})
+})
+
+router.get('/get_ali_zkl_js',function(req,res,next){
+	mem.get('get_ali_zkl_liujiazhi').then(function(value){
+		//console.log('========get_zkl_js mem=======');
+		if(value){
+			res.send('get_zkl_js('+value+')')
+		}else{
+			var url = "https://www.guojucloud.com/zkl/ali/xieb.json";
+			request({url: url, timeout: 3000}, function (err, response, body) {
+				if(err){
+					console.log(err)
+					return res.send('error');
+				}
+				try{
+					body = body.split('(')[1].split(')')[0]
+		            var data = JSON.parse(body);
+		            var zkl = [];
+		            zkl = data["code"];
+		            mem.set('get_ali_zkl_liujiazhi',JSON.stringify(zkl),60).then(function(){});
+					var text = 'get_zkl_js('+JSON.stringify(zkl)+')';
+					res.send(text)
+				}catch(e){
+					//console.log(e)
+					res.send('error');
+				}
+			});
+		}
+	})
+})
 
 module.exports = router;
