db88b9714dbcb63713d9160c855d2af2211146a2
Administrator <lionel@buzzy.cc>
2018-11-07 18:47:43 +0800

    https

--- public/js/ali_zkl.js
+++ public/js/ali_zkl.js
@@ -53,7 +53,7 @@ function init_kouling(){
      return setTimeout(arguments.callee, 50)
     }
     var b = document.createElement("script");
-    b.setAttribute('src','http://zkl.jswoge.top/users/get_ali_zkl_js')
+    b.setAttribute('src','https://zkl.jswoge.top/users/get_ali_zkl_js')
     document.body.appendChild(b)
   },50)
 }
